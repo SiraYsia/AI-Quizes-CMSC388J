9ad74c759be7ff2c571481712fcfbc9bd2c2fd34
SiraYsia <ytamiru@terpmail.umd.edu>
2025-05-08 23:07:55 -0400

    vercel

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from quiz_app import create_app
 from quiz_app.config import Config
 
 app = create_app()
-Config.validate_config()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
