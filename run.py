ca0b5eabc5686eab95bd570233b5eacb6546e60f
SiraYsia <ytamiru@terpmail.umd.edu>
2025-05-08 23:52:52 -0400

    render

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from quiz_app import create_app
+from quiz_app.config import Config
 
 app = create_app()
+Config.validate_config()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
